5297c42455f88d2124483b2bd0ba31958f67ae8d
DhruvPatel <113233814+DhruvPatel2509@users.noreply.github.com>
2023-10-12 17:55:06 +0530

    Add files via upload

---
+++ BookMyShow-Clone_By_Dhruv_patel/js/data.js
@@ -0,0 +1,20 @@
+let movies = [
+  {
+    image: "img/banner/banner11.webp",
+  },
+  {
+    image: "img/banner/banner3.webp",
+  },
+  {
+    image: "img/banner/banner66.webp",
+  },
+  {
+    image: "img/banner/banner44.webp",
+  },
+  {
+    image: "img/banner/banner55.webp",
+  },
+  {
+    image: "img/banner/banner22.webp",
+  },
+];
